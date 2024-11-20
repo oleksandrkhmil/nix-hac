5498460c4b54d289b53d6e36c419bc7ae3736364
oleksandrkhmil <oleksandr.khmil@sumup.com>
2024-11-20 21:58:04 +0200

    fff

--- index.js
+++ index.js
@@ -50,7 +50,7 @@ app.get('/healthz', (req, res) => {
 
 const PORT = 8000;
 app.listen(PORT, () => {
-    // console.log(`Server started on port ${PORT}`);
+    console.log(`Server started on port ${PORT}`);
 });
 
 /**
